c99bf2e3d2cc26de3e646b39df284277b304b7a3
NKBolg <nkbolg@gmail.com>
2020-02-28 00:19:15 +0300

    moved logging to decorator

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import functools
 import pychromecast
 import logging
 import os
@@ -14,6 +15,18 @@ proxy = {
 }
 
 
+def _log(func):
+    @functools.wraps(func)
+    def magic(self, *args, **kwargs):
+        assert self.selected_cast is not None
+        res = func(self, *args, **kwargs)
+        logging.info(func.__name__ + ' returned')
+        logging.debug(self.selected_cast.media_controller.status)
+        logging.debug(self.selected_cast.status)
+        return res
+    return magic
+
+
 class PlayerController:
     def __init__(self):
         self.chromecasts = pychromecast.get_chromecasts()
@@ -22,52 +35,38 @@ class PlayerController:
         self.selected_cast = None
 
     def select(self, idx):
-        assert 0 <= idx <= len(self.chromecasts)
+        assert 0 <= idx < len(self.chromecasts)
         self.selected_cast = self.chromecasts[idx]
 
+    @_log
     def play_from_start(self, url):
-        assert self.selected_cast is not None
         self.selected_cast.wait()
         logging.debug(self.selected_cast.status)
         mc = self.selected_cast.media_controller
         mc.play_media(url, 'audio/mp3')
         mc.block_until_active()
         mc.play()
-        logging.info('playing')
-        logging.debug(self.selected_cast.media_controller.status)
-        logging.debug(self.selected_cast.status)
 
+    @_log
     def playpause(self):
-        assert self.selected_cast is not None
         if self.selected_cast.media_controller.status.player_state == 'PLAYING':
             self.selected_cast.media_controller.pause()
             logging.info('pause')
         else:
             self.selected_cast.media_controller.play()
             logging.info('continue')
-        logging.debug(self.selected_cast.media_controller.status)
-        logging.debug(self.selected_cast.status)
 
+    @_log
     def stop(self):
-        assert self.selected_cast is not None
         self.selected_cast.media_controller.stop()
-        logging.info('stop')
-        logging.debug(self.selected_cast.media_controller.status)
-        logging.debug(self.selected_cast.status)
 
+    @_log
     def volume_up(self):
-        assert self.selected_cast is not None
         self.selected_cast.volume_up()
-        logging.info('volume_up')
-        logging.debug(self.selected_cast.media_controller.status)
-        logging.debug(self.selected_cast.status)
 
+    @_log
     def volume_down(self):
-        assert self.selected_cast is not None
         self.selected_cast.volume_down()
-        logging.info('volume_down')
-        logging.debug(self.selected_cast.media_controller.status)
-        logging.debug(self.selected_cast.status)
 
 
 class BotController:
